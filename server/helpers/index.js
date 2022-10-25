c23dc130a2fb0d8593b9eb0d5ae29dd0e58a79ca
m7rlin <merlin@magictm.com>
2022-10-25 23:53:33 +0200

    fix copy paste error

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -45,46 +45,8 @@ const getFullPopulateObject = (modelUid, maxDepth = 20) => {
                 populate[key] = true
             }
         }
-
-        if (modelUid === 'admin::user') {
-            return undefined
-        }
-
-        const populate = {}
-        const model = strapi.getModel(modelUid)
-
-        // console.log('model', Object.entries(getModelPopulationAttributes(model)))
-        for (const [key, value] of Object.entries(
-            getModelPopulationAttributes(model),
-        )) {
-            if (value) {
-                if (value.type === 'component') {
-                    populate[key] = getFullPopulateObject(
-                        value.component,
-                        maxDepth - 1,
-                    )
-                } else if (value.type === 'dynamiczone') {
-                    const dynamicPopulate = value.components.reduce((prev, cur) => {
-                        const curPopulate = getFullPopulateObject(cur, maxDepth - 1)
-                        return curPopulate === true ? prev : merge(prev, curPopulate)
-                    }, {})
-                    populate[key] = isEmpty(dynamicPopulate) ? true : dynamicPopulate
-                } else if (value.type === 'relation') {
-                    const relationPopulate = getFullPopulateObject(
-                        value.target,
-                        key === 'localizations' && maxDepth > 2 ? 1 : maxDepth - 1,
-                    )
-                    if (relationPopulate) {
-                        populate[key] = relationPopulate
-                    }
-                } else if (value.type === 'media') {
-                    populate[key] = true
-                }
-            }
-        }
-
-        return isEmpty(populate) ? true : { populate }
     }
+    return isEmpty(populate) ? true : { populate }
 }
 
 module.exports = {
